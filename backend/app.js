626a6ad0caedfe790025082af769ab4900608f54
marceldavid <marceldavidbaroi@gmail.com>
2024-03-02 03:43:11 +0600

    order module has not been tested

--- backend/app.js
+++ backend/app.js
@@ -14,9 +14,11 @@ app.use(cookieParser());
 //import all routes
 const products = require('./routes/product.js');
 const auth = require('./routes/auth.js');
+const order = require('./routes/order.js');
 
 app.use('/api/v1',products);
 app.use('/api/v1',auth);
+app.use('/api/v1',order);
 
 
 //Middleware to handle errors
